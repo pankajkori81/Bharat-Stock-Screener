c29378557e023f10cd9d69e8468e756278044989
Pankaj kori <93833439+pankajkori81@users.noreply.github.com>
2025-10-26 21:18:39 +0530

    Update routes and add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
-
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import ContactPage from './pages/ContactPage';
 import FeedbackPage from './pages/FeedbackPage';
 import  ThemeProvider from './context/ThemeContext';
+import Footer from './components/Footer';
+import PrivacyPolicyPage from './support/PrivacyPolicyPage';
+import Terms from './support/Terms';
+import AboutPage from './support/AboutPage';
 
 const App=()=> {
   return (
@@ -15,9 +17,13 @@ const App=()=> {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/feedback" element={<FeedbackPage />} />
+          <Route path="/pages/contact" element={<ContactPage />} />
+          <Route path="/pages/feedback" element={<FeedbackPage />} />
+          <Route path="/support/about" element={<AboutPage />} />
+          <Route path="/support/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="/support/terms-conditions" element={<Terms />} />
         </Routes>
+        <Footer/>
       </Router>
     </ThemeProvider>
   );
