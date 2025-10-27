e1cd7055fc07d9d8c2123dd66ccbab12978e32e0
Pankaj kori <93833439+pankajkori81@users.noreply.github.com>
2025-10-27 13:45:11 +0530

    Remove HelmetProvider from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
 
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <Router>
-        <App />
-      </Router>
-    </HelmetProvider>
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
 )
