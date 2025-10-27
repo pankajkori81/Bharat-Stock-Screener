6a492a6029b65877ded67387cb9b9776b3062d00
Pankaj kori <93833439+pankajkori81@users.noreply.github.com>
2025-10-27 13:23:09 +0530

    Refactor App component structure and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Header from './components/Header'
+import ContactPage from './pages/ContactPage'
+import FeedbackPage from './pages/FeedbackPage'
+import ThemeProvider from './context/ThemeContext'
+import Footer from './components/Footer'
+import PrivacyPolicyPage from './support/PrivacyPolicyPage'
+import Terms from './support/Terms'
+import AboutPage from './support/AboutPage'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Header from './components/Header';
-import ContactPage from './pages/ContactPage';
-import FeedbackPage from './pages/FeedbackPage';
-import ThemeProvider from './context/ThemeContext';
-import Footer from './components/Footer';
-import PrivacyPolicyPage from './support/PrivacyPolicyPage';
-import Terms from './support/Terms';
-import AboutPage from './support/AboutPage';
-
-const App=()=> {
+const App = () => {
   return (
     <ThemeProvider>
-      <Router>
-        <Header/>
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/pages/contact" element={<ContactPage/>} />
-          <Route path="/pages/feedback" element={<FeedbackPage/>} />
-          <Route path="/support/about" element={<AboutPage/>} />
-          <Route path="/support/privacy-policy" element={<PrivacyPolicyPage/>} />
-          <Route path="/support/terms-conditions" element={<Terms/>} />
-        </Routes>
-        <Footer/>
-      </Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pages/contact" element={<ContactPage />} />
+        <Route path="/pages/feedback" element={<FeedbackPage />} />
+        <Route path="/support/about" element={<AboutPage />} />
+        <Route path="/support/privacy-policy" element={<PrivacyPolicyPage />} />
+        <Route path="/support/terms-conditions" element={<Terms />} />
+      </Routes>
+      <Footer />
     </ThemeProvider>
-  );
+  )
 }
-export default App;
+
+export default App
